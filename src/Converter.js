945dfd4dc409b64940d88bffd38ca8315dc2f2de
Izaac Abrams <52928596+izaacAbrams@users.noreply.github.com>
2020-09-03 12:12:52 -0500

    added download, title, and iframe of link

--- src/Converter.js
+++ src/Converter.js
@@ -1,21 +1,44 @@
 import React, {Component} from 'react'
-import ApiService from './services/api-service'
 class Converter extends Component {
+    state = {
+        title: null,
+        embed: null
+    }
+    handleDownload(e) {
+        e.preventDefault()
+        const URL = `https://www.youtube.com/watch?v=IBDgHaTy_wU`
+        window.location.href = `http://localhost:8000/download?URL=${URL}`;
+    }
 
+    getInfo() {
+        return fetch(`http://localhost:8000/info?URL=https://www.youtube.com/watch?v=IBDgHaTy_wU`, {
+            method: 'GET'
+        }).then((res) => 
+            !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json())
+    }
     handleSubmit(e) {
         e.preventDefault()
-        ApiService.getDownload()
-        // browser.downloads.download({
-        //     url: 'https://www.youtube.com/watch?v=IBDgHaTy_wU',
-        //     filename: "music.mp3"
-        // })
+        console.log(document.querySelector('.Converter__input').value)
+        this.getInfo().then(info => {
+            this.setState({
+                title: info.videoDetails.title,
+                embed: info.videoDetails.embed.iframeUrl
+            })
+           
+        })
+    
     }
+
     render() {
         return (
             <div className='Converter'>
-                <input type='text'/>
-                <button type="file">Choose Folder</button>
-                <a href="/" download onClick={e => this.handleSubmit(e)}>Convert</a>
+                <form onSubmit={e => this.handleSubmit(e)}>
+                <input className="Converter__input" type='text'/>
+                <button type="submit">Find Video</button>
+                </form>
+                {this.state.title ? this.state.title : <></>}
+                {this.state.embed ? <iframe src={this.state.embed} title={this.state.title}/> : <></>}
+                <a href="/" download onClick={e => this.handleDownload(e)}>Download</a>
             </div>
         )
     }
