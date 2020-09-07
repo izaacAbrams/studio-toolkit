889944ad36f961fcf562ec448152b1cf327766a3
Izaac Abrams <52928596+izaacAbrams@users.noreply.github.com>
2020-09-07 14:01:35 -0500

    added styles

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import Converter from './Converter'
-import './App.css';
+import React from "react";
+import Converter from "./Converter";
+import logo from "./images/logo.jpg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <Converter />
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="App__nav">
+				<img src={logo} className="App__logo" alt="TM Studioz Logo" />
+				<h1 className="App__header">TM Studioz Converter</h1>
+			</div>
+			<Converter />
+		</div>
+	);
 }
 
 export default App;
--- src/Converter.js
+++ src/Converter.js
@@ -1,47 +1,88 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 class Converter extends Component {
-    state = {
-        title: null,
-        embed: null
-    }
-    handleDownload(e) {
-        e.preventDefault()
-        const URL = `https://www.youtube.com/watch?v=IBDgHaTy_wU`
-        window.location.href = `http://localhost:8000/download?URL=${URL}`;
-    }
+	state = {
+		title: null,
+		embed: null,
+		error: null,
+	};
+	handleDownload(e) {
+		e.preventDefault();
+		const URL = `https://www.youtube.com/watch?v=IBDgHaTy_wU`;
+		window.location.href = `http://localhost:8000/download?URL=${URL}`;
+	}
 
-    getInfo() {
-        return fetch(`http://localhost:8000/info?URL=https://www.youtube.com/watch?v=IBDgHaTy_wU`, {
-            method: 'GET'
-        }).then((res) => 
-            !res.ok ? res.json().then((e) => Promise.reject(e)) : res.json())
-    }
-    handleSubmit(e) {
-        e.preventDefault()
-        console.log(document.querySelector('.Converter__input').value)
-        this.getInfo().then(info => {
-            this.setState({
-                title: info.videoDetails.title,
-                embed: info.videoDetails.embed.iframeUrl
-            })
-           
-        })
-    
-    }
+	getInfo() {
+		const search = document.querySelector(".Converter__input").value;
+		return fetch(`http://localhost:8000/info?URL=${search}`, {
+			method: "GET",
+		}).then((res) =>
+			!res.ok ? res.json().then((e) => Promise.reject(e)) : res.json()
+		);
+	}
+	handleSubmit(e) {
+		e.preventDefault();
+		console.log(
+			document.querySelector(".Converter__input").value.trim().length === 0
+		);
+		if (document.querySelector(".Converter__input").value.trim().length === 0) {
+			this.setState({
+				error: "Please paste URL into search bar",
+			});
+		} else {
+			this.setState({
+				error: null,
+			});
+			this.getInfo().then((info) => {
+				this.setState({
+					title: info.videoDetails.title,
+					embed: info.videoDetails.embed.iframeUrl,
+				});
+			});
+		}
+	}
 
-    render() {
-        return (
-            <div className='Converter'>
-                <form onSubmit={e => this.handleSubmit(e)}>
-                <input className="Converter__input" type='text'/>
-                <button type="submit">Find Video</button>
-                </form>
-                {this.state.title ? this.state.title : <></>}
-                {this.state.embed ? <iframe src={this.state.embed} title={this.state.title}/> : <></>}
-                <a href="/" download onClick={e => this.handleDownload(e)}>Download</a>
-            </div>
-        )
-    }
+	render() {
+		const error = !!this.state.error ? (
+			<p className="error">{this.state.error}</p>
+		) : (
+			<></>
+		);
+		return (
+			<div className="Converter">
+				<form onSubmit={(e) => this.handleSubmit(e)}>
+					<input className="Converter__input" type="text" />
+					<button className="Converter__find_btn" type="submit">
+						Convert
+					</button>
+				</form>
+				{!!this.state.title ? (
+					<h2 className="Converter__vid_title">{this.state.title}</h2>
+				) : (
+					<></>
+				)}
+				{error}
+				{!!this.state.embed ? (
+					<div className="Converter__container">
+						<iframe
+							src={this.state.embed}
+							className="Converter__video"
+							title={this.state.title}
+						/>
+						<a
+							href="/"
+							className="Converter__download"
+							download
+							onClick={(e) => this.handleDownload(e)}
+						>
+							Download
+						</a>
+					</div>
+				) : (
+					<></>
+				)}
+			</div>
+		);
+	}
 }
 
-export default Converter
+export default Converter;
